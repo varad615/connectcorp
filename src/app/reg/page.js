432fbe8613956d4a1dba26e3d89334066b56c690
Varad Gundap <30592638+varad615@users.noreply.github.com>
2023-07-04 00:22:04 +0530

    page fix

---
+++ src/app/reg/page.js
@@ -0,0 +1,103 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FcGoogle } from "react-icons/fc";
+export default function reg() {
+  return (
+    <>
+      <div>
+        <section class="min-h-screen flex items-stretch text-white ">
+          <div class="lg:flex w-1/2 hidden bg-no-repeat bg-cover relative items-center bg-gradient-to-b from-purple-800 to-indigo-800">
+            <div class="w-full px-24 z-10">
+              <h1 class="text-5xl font-bold text-left tracking-wide">
+                Speedy, Easy and Fast
+              </h1>
+            </div>
+          </div>
+          <div class="lg:w-1/2 md:w-1/2 w-full flex items-center justify-center text-center md:px-16 px-0 z-0">
+            <div class="w-full py-6 z-20">
+              <form class="bg-white items-center flex flex-col">
+                <div className="text-black text-[35px] font-bold">
+                  Sign in for an account
+                </div>
+                <button
+                  type="button"
+                  class="text-[#484B51] border border-[#BCC2D2] mt-7 bg-white font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2"
+                >
+                  <FcGoogle size={20} className="mr-2" />
+                  Sign in with Google
+                </button>
+                <div className="w-[446px] h-5 mt-3 relative">
+                  <div className="left-[167px] top-0 absolute text-neutral-600 text-[16px] font-medium">
+                    Or with email
+                  </div>
+                  <div className="w-[154px] h-[0px] left-[280px] top-[11px] absolute border border-neutral-600"></div>
+                  <div className="w-[154px] h-[0px] left-0 top-[11px] absolute border border-neutral-600"></div>
+                </div>
+
+                <div className="w-full flex flex-col gap-3 mt-3">
+                  <div class="sm:col-span-2 w-full">
+                    <label
+                      for="name"
+                      class="block mb-2 text-sm font-medium text-gray-900 "
+                    >
+                      Username *
+                    </label>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      class=" border border-[#BCC2D2] text-black bg-white text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                    />
+                  </div>
+
+                  <div class="sm:col-span-2 w-full">
+                    <label
+                      for="price"
+                      class="block mb-2 text-sm font-medium text-gray-900"
+                    >
+                      Password *
+                    </label>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      class=" border border-[#BCC2D2] text-black bg-white text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                    />
+                  </div>
+                </div>
+                <div className="w-[458px] h-[25px] relative mt-5">
+                  <div className="left-[30px] top-[2px] absolute text-black text-[16px] font-semibold tracking-wide">
+                    Remember Me
+                  </div>
+                  <div className="w-[25px] h-[25px] left-0 top-0 absolute justify-center items-center inline-flex">
+                    <input
+                      id="c1"
+                      type="checkbox"
+                      class="h-4 w-4 cursor-pointer rounded-full border-indigo-600 bg-indigo-100 text-[#475FE2]"
+                    />
+                  </div>
+                  <div className="left-[308px] top-[2px] absolute text-indigo-600 text-[16px] font-semibold tracking-wide">
+                    Forgot Password?
+                  </div>
+                </div>
+
+                <button
+                  type="submit"
+                  class="text-white bg-[#475FE2] mt-7 hover:bg-primary-800 w-full font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                >
+                  Sign Up
+                </button>
+                <Link href="/register">
+                  <div className="w-[458px] text-[#484B51] text-base mt-7 text-center">
+                    Already have an account?{" "}
+                    <span className="font-bold">Sign In</span>
+                  </div>
+                </Link>
+              </form>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { FcGoogle } from "react-icons/fc";
+import { AiFillApple } from "react-icons/ai";
 export default function login() {
   return (
     <>
@@ -10,7 +11,7 @@ export default function login() {
             <div>
               <Image
                 priority
-                src="/register.svg"
+                src="/register.png"
                 height={300}
                 width={300}
                 alt="Follow us on Twitter"
@@ -20,106 +21,124 @@ export default function login() {
               </h1>
             </div>
           </div>
-          <div class="flex md:w-1/2 justify-center py-10 items-center bg-white">
-            <form class="bg-white items-center flex flex-col">
-              <div className="text-black text-[35px] font-bold">
-                Sign up for an account
-              </div>
-              <button
-                type="button"
-                class="text-[#484B51] border border-[#BCC2D2] mt-7 bg-white font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2"
-              >
-                <FcGoogle size={20} className="mr-2" />
-                Sign up with Google
-              </button>
-              <div className="w-[446px] h-5 mt-3 relative">
-                <div className="left-[167px] top-0 absolute text-neutral-600 text-[16px] font-medium">
-                  Or with email
-                </div>
-                <div className="w-[154px] h-[0px] left-[280px] top-[11px] absolute border border-neutral-600"></div>
-                <div className="w-[154px] h-[0px] left-0 top-[11px] absolute border border-neutral-600"></div>
-              </div>
-
-              <div class="grid mt-7 gap-4 sm:grid-cols-2">
+          <div class="flex md:w-1/2 sm:p-3 justify-center py-10 items-center bg-white">
+            <div class="max-w-2xl bg-white py-10 px-5 m-auto w-full">
+              <div class="text-3xl mb-6 font-bold text-center text-black">
+                <div>Sign up for an account</div>
                 <div>
-                  <label
-                    for="name"
-                    class="block mb-2 text-sm font-medium text-gray-900 "
+                  <button
+                    type="button"
+                    class="text-[#484B51] border border-[#BCC2D2] mt-7 bg-white font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2"
                   >
-                    First Name *
-                  </label>
-                  <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    class=" border border-[#BCC2D2] text-black bg-white text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                  />
-                </div>
-                <div>
-                  <label
-                    for="name"
-                    class="block mb-2 text-sm font-medium text-gray-900 "
+                    <FcGoogle size={20} className="mr-2" />
+                    Sign up with Google
+                  </button>
+                  <button
+                    type="button"
+                    class="text-[#484B51] border border-[#BCC2D2] mt-7 bg-white font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2"
                   >
-                    Last Name *
-                  </label>
-                  <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    class=" border border-[#BCC2D2] text-black bg-white text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                  />
+                    <AiFillApple size={20} className="mr-2" />
+                    Sign up with Apple
+                  </button>
                 </div>
-                <div class="sm:col-span-2">
-                  <label
-                    for="name"
-                    class="block mb-2 text-sm font-medium text-gray-900 "
-                  >
-                    Username *
-                  </label>
-                  <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    class=" border border-[#BCC2D2] text-black bg-white text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                  />
-                </div>
-
-                <div class="sm:col-span-2">
-                  <label
-                    for="price"
-                    class="block mb-2 text-sm font-medium text-gray-900"
-                  >
-                    Password *
-                  </label>
-                  <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    class=" border border-[#BCC2D2] text-black bg-white text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                  />
-                </div>
-              </div>
-              <div className="w-[458px] text-[#484B51] text-xs mt-7">
-                By creating an account, you are agreeing to our{" "}
-                <span className="font-bold">Privacy policy</span> and{" "}
-                <span className="font-bold">
-                  Electronic Communication Policy.
-                </span>
+                <hr class="w-48 h-1 mx-auto my-2 bg-gray-100 border-0 rounded md:my-5 dark:bg-gray-700" />
               </div>
 
-              <button
-                type="submit"
-                class="text-white bg-[#475FE2] mt-7 hover:bg-primary-800 w-full font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-              >
-                Sign Up
-              </button>
-              <Link href="/login">
-                <div className="w-[458px] text-[#484B51] text-base mt-7 text-center">
-                  Already have an account?{" "}
-                  <span className="font-bold">Sign In</span>
+              <form>
+                <div class="grid grid-cols-2 gap-4 max-w-xl m-auto">
+                  <div class="col-span-2 lg:col-span-1">
+                    <div class="sm:col-span-2 w-full">
+                      <label
+                        for="name"
+                        class="block mb-2 text-sm font-medium text-gray-900 "
+                      >
+                        First Name *
+                      </label>
+                      <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        class=" border border-[#BCC2D2] text-black bg-white text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                      />
+                    </div>
+                  </div>
+
+                  <div class="col-span-2 lg:col-span-1">
+                    <div class="sm:col-span-2 w-full">
+                      <label
+                        for="price"
+                        class="block mb-2 text-sm font-medium text-gray-900"
+                      >
+                        Last Name *
+                      </label>
+                      <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        class=" border border-[#BCC2D2] text-black bg-white text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                      />
+                    </div>
+                  </div>
+
+                  <div class="col-span-2">
+                    <div class="sm:col-span-2 w-full">
+                      <label
+                        for="price"
+                        class="block mb-2 text-sm font-medium text-gray-900"
+                      >
+                        Username *
+                      </label>
+                      <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        class=" border border-[#BCC2D2] text-black bg-white text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                      />
+                    </div>
+                  </div>
+                  <div class="col-span-2">
+                    <div class="sm:col-span-2 w-full">
+                      <label
+                        for="price"
+                        class="block mb-2 text-sm font-medium text-gray-900"
+                      >
+                        Password *
+                      </label>
+                      <input
+                        type="password"
+                        name="name"
+                        id="name"
+                        class=" border border-[#BCC2D2] text-black bg-white text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                      />
+                    </div>
+                  </div>
+                  <div class="col-span-2 text-[#484B51]">
+                    By creating an account, you are agreeing to our{" "}
+                    <span className="font-bold">Privacy policy</span> and{" "}
+                    <span className="font-bold">
+                      Electronic Communication Policy.
+                    </span>
+                  </div>
+                  <div class="col-span-2 text-right">
+                    <button
+                      type="submit"
+                      class="text-white bg-[#475FE2] mt-5 hover:bg-primary-800 w-full font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                    >
+                      Sign Up
+                    </button>
+                  </div>
+                  <div class="col-span-2 text-right"></div>
                 </div>
-              </Link>
-            </form>
+              </form>
+              <div class="text-base mb-6 text-center text-black">
+                <Link href="/login">
+                  <div>
+                    Already have an account?{" "}
+                    <span className="font-bold">Sign In</span>{" "}
+                  </div>
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
